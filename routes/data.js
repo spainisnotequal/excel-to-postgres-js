409c2eff1a60d07a776e31af1f8cde07e11d354d
spainisnotequal <spainisnotequal@gmail.com>
2020-04-27 18:36:34 +0200

    Add POST / route

--- routes/data.js
+++ routes/data.js
@@ -13,9 +13,27 @@ const router = express.Router();
 // @description  Get all data from the database
 // @acces        Public
 router.get("/", (request, response) => {
+  const query = "SELECT * FROM money_in";
   const pool = new Pool();
   pool
-    .query("SELECT * FROM money_in")
+    .query(query)
+    .then((res) => response.json(res.rows))
+    .catch((err) => response.json(err));
+});
+
+// @route        POST /api/
+// @description  Post a new record to the database
+// @acces        Public
+router.post("/", (request, response) => {
+  const date = request.body.date;
+  const concept = request.body.concept;
+  const amount = request.body.amount;
+
+  const query = `INSERT INTO money_in (date, concept, amount) VALUES ('${date}', '${concept}', ${amount})`;
+
+  const pool = new Pool();
+  pool
+    .query(query)
     .then((res) => response.json(res.rows))
     .catch((err) => response.json(err));
 });
